013ff7b51dcc53d00b9e5d2e01996c8bc1d86771
Nicholas Wiersma <nick@wiersma.co.za>
2016-09-09 08:42:12 +0200

    Version bump

--- examples/simple.ino
+++ examples/simple.ino
@@ -1,6 +1,7 @@
 #include "ConfigManager.h"
 
 struct Config {
+    char name[20];
     bool enabled;
     int8 hour;
 } config;
@@ -11,6 +12,7 @@ void setup() {
     // Setup config manager
     configManager.setAPName("Demo");
     configManager.setAPFilename("/index.html");
+    configManager.addParameter("name", &config.name, 20);
     configManager.addParameter("enabled", &config.enabled);
     configManager.addParameter("hour", &config.hour);
     configManager.begin(config);
