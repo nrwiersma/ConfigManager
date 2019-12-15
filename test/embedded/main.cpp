46505b10dd05adff24cdbd6f7f28ef1e0373e422
Yomi Colledge <baphled@boodah.net>
2019-12-15 05:58:40 +0000

    Add tests for ConfigStringParameter class

--- test/embedded/main.cpp
+++ test/embedded/main.cpp
@@ -4,15 +4,23 @@
 #include <unity.h>
 
 #include "ConfigParameterTest.h"
+#include "ConfigStringParameterTest.h"
 
 void setup() {
   delay(2000);
   UNITY_BEGIN();
 
+  // ConfigParameterTest
   RUN_TEST(test_sets_config_parameter_mode);
   RUN_TEST(test_config_parameter_stored_as_json);
   RUN_TEST(test_config_parameter_stored_from_json);
   RUN_TEST(test_config_parameter_clear_data);
+
+  // ConfigStringParameterTest
+  RUN_TEST(test_sets_config_string_parameter_mode);
+  RUN_TEST(test_config_string_parameter_stored_as_json);
+  RUN_TEST(test_config_string_parameter_stored_from_json);
+  RUN_TEST(test_config_string_parameter_clear_data);
 
   UNITY_END();
 }
